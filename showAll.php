3fc7ec7738002fde2c6397e2d71cc48526ac2e0b
Kate Lewis <lewiskaitlynj@gmail.com>
2021-10-14 12:38:39 -0400

    audio implemented

--- showAll.php
+++ showAll.php
@@ -30,26 +30,27 @@
 
         <div class="audioPlayer">
             <div class="player">
-
+            <audio controls autoplay loop id="audio" ></audio>
                 <!-- Define the section for displaying play button -->
-                <div id="audio" class="playpause-track" onclick="playpauseTrack()">
+                <!-- <div class="playpause-track" onclick="playpauseTrack()">
+                    <audio id="audio" ></audio>
                     <i class="fa fa-play-circle fa-5x"></i>
-                </div>
+                </div> -->
 
 
                 <!-- Define the section for displaying the seek slider-->
-                <div class="slider_container">
+                <!-- <div class="slider_container">
                     <div class="current-time">00:00</div>
                     <input type="range" min="1" max="100" value="0" class="seek_slider" onchange="seekTo()">
                     <div class="total-duration">00:00</div>
-                </div>
+                </div> -->
 
                 <!-- Define the section for displaying the volume slider-->
-                <div class="slider_container">
+                <!-- <div class="slider_container">
                     <i class="fa fa-volume-down"></i>
                     <input type="range" min="1" max="100" value="99" class="volume_slider" onchange="setVolume()">
                     <i class="fa fa-volume-up"></i>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -1,6 +1,6 @@
 const posts = [];
 let currentSlide = 0;
-let curr_track = document.getElementById("audio");
+
 
 //connet to API and create console log the data
 fetch('https://multisite.local/eco-martyrs/wp-json/wp/v2/ecomartyrs?_embed=true')
@@ -63,10 +63,14 @@ fetch('https://multisite.local/eco-martyrs/wp-json/wp/v2/ecomartyrs?_embed=true'
     let image = document.getElementById("featuredImage");
     image.src = post._embedded['wp:featuredmedia']['0'].source_url;  
 
-    //Audio    
+    //Audio   
+    let curr_track = document.getElementById("audio"); 
     curr_track.src = post.acf.audio_file;
-    //console.log(post.acf.audio_file)
-    return curr_track.src;
+    const music = new Audio(curr_track.src);
+    music.play();
+    music.loop =true;
+    music.playbackRate = 2;
+    music.pause();
     }
 
    //Show Next Slide
@@ -92,109 +96,4 @@ fetch('https://multisite.local/eco-martyrs/wp-json/wp/v2/ecomartyrs?_embed=true'
     //        AUDIO PLAYER       //
     //**************************//
 
-    // Select all the elements in the HTML page and assign them to a variable
-    //let now_playing = document.querySelector(".now-playing");
-    //let track_name = document.querySelector(".track-name");
-    //let track_artist = document.querySelector(".track-artist");
-    let playpause_btn = document.querySelector(".playpause-track");
-    let seek_slider = document.querySelector(".seek_slider");
-    let volume_slider = document.querySelector(".volume_slider");
-    let curr_time = document.querySelector(".current-time");
-    let total_duration = document.querySelector(".total-duration");
-
-    // Specify globally used values
-    let track_index = 0;
-    let isPlaying = true;
-    let updateTimer;
-
-    // Create the audio element for the player 
-      //curr_track.load();
-        
-    //Load a new track    
-    function loadTrack(track_index) {
-      // Clear the previous seek timer
-      clearInterval(updateTimer);
-      resetValues();
-      // Set an interval of 1000 milliseconds
-      // for updating the seek slider
-      updateTimer = setInterval(seekUpdate, 1000);
-    }
-
-    // Function to reset all values to their default
-      function resetValues() {
-      curr_time.textContent = "00:00";
-      total_duration.textContent = "00:00";
-      seek_slider.value = 0;
-    }
-
-    function playpauseTrack() {
-    // Switch between playing and pausing
-    // depending on the current state
-        if (!isPlaying){
-       playTrack();    
-     }else pauseTrack();
-  
-    }
-    function playTrack() {
-    // Play the loaded track
-    curr_track.src.play();    
-    isPlaying = true;
-    // Replace icon with the pause icon
-    playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
-    }
-
-    function pauseTrack() {
-      console.log(curr_track.src)
-    // Pause the loaded track
-    curr_track.src.pause();
-    isPlaying = false;
-    // Replace icon with the play icon
-    playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
-    }
-
-   
-    // function seekTo() {
-    // // Calculate the seek position by the
-    // // percentage of the seek slider
-    // // and get the relative duration to the track
-    // seekto = curr_track.duration * (seek_slider.value / 100);
-
-    // // Set the current track position to the calculated seek position
-    // curr_track.currentTime = seekto;
-    // }
-
-    // function setVolume() {
-    // // Set the volume according to the
-    // // percentage of the volume slider set
-    // curr_track.volume = volume_slider.value / 100;
-    // }
-
-    // function seekUpdate() {
-    // let seekPosition = 0;
-
-    // // Check if the current track duration is a legible number
-    // if (!isNaN(curr_track.duration)) {
-    //   seekPosition = curr_track.currentTime * (100 / curr_track.duration);
-    //   seek_slider.value = seekPosition;
-
-    //   // Calculate the time left and the total duration
-    //   let currentMinutes = Math.floor(curr_track.currentTime / 60);
-    //   let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
-    //   let durationMinutes = Math.floor(curr_track.duration / 60);
-    //   let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
-
-    //   // Add a zero to the single digit time values
-    //   if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
-    //   if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
-    //   if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
-    //   if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
-
-    //   // Display the updated duration
-    //   curr_time.textContent = currentMinutes + ":" + currentSeconds;
-    //   total_duration.textContent = durationMinutes + ":" + durationSeconds;
-    // }
-    // }
-
-    // Load the first track in the tracklist
-    //loadTrack(curr_track);
- 
+    
