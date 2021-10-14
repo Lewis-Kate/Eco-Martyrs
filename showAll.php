8342838aa8a1916ded19b6e333fc2c94fef0fff8
Kate Lewis <lewiskaitlynj@gmail.com>
2021-10-14 13:35:23 -0400

    additionalLinks line break

--- showAll.php
+++ showAll.php
@@ -29,29 +29,7 @@
         </div>
 
         <div class="audioPlayer">
-            <div class="player">
-            <audio controls autoplay loop id="audio" ></audio>
-                <!-- Define the section for displaying play button -->
-                <!-- <div class="playpause-track" onclick="playpauseTrack()">
-                    <audio id="audio" ></audio>
-                    <i class="fa fa-play-circle fa-5x"></i>
-                </div> -->
-
-
-                <!-- Define the section for displaying the seek slider-->
-                <!-- <div class="slider_container">
-                    <div class="current-time">00:00</div>
-                    <input type="range" min="1" max="100" value="0" class="seek_slider" onchange="seekTo()">
-                    <div class="total-duration">00:00</div>
-                </div> -->
-
-                <!-- Define the section for displaying the volume slider-->
-                <!-- <div class="slider_container">
-                    <i class="fa fa-volume-down"></i>
-                    <input type="range" min="1" max="100" value="99" class="volume_slider" onchange="setVolume()">
-                    <i class="fa fa-volume-up"></i>
-                </div> -->
-            </div>
+            <audio controls autoplay loop id="audio"></audio>
         </div>
     </div>
 
@@ -60,8 +38,6 @@
     </div>
 
     <button id="showNext" onclick="showNextSlide()"><i class="fas fa-chevron-right"></i></button>
-
-
 
 </div>
 
