e9074a4bb306d5bfbda1d05a40b21aa7f485f3ec
Kate Lewis <lewiskaitlynj@gmail.com>
2021-11-11 12:22:26 -0500

    ongoing research content

--- about_the_research.php
+++ about_the_research.php
@@ -5,4 +5,62 @@
     <h1>Ongoing Research</h1>
 </div>
 
+<div class="aboutContent">
+    <div class="aboutParagraphs">
+        <p>For the last five years, my research team has been writing summaries based on a list of environmentalists,
+            indigenous
+            peoples, farmers and other community leaders who have been murdered defending the earth that we all share.
+            The
+            Global Witness Organization complies and releases a yearly list of names that is international in scope.
+            There have
+            been about 2000 confirmed deaths of “eco-martyrs” since 2002. These activists have been working to stop
+            projects
+            that would lead to environmental destruction, displacement of indigenous people, increased climate change,
+            extinction of animals and resources or elimination of prior ways of life.</p>
+
+        <p>These persons have stood up to governments, militaries, corporations and local police despite the efforts of
+            those
+            entities to intimidate them. Most of the eco-martyrs had been previously threatened with violence and/or
+            their own
+            murder. Our research attempts to humanize what otherwise would be a long list of murdered individuals. We
+            write
+            short summaries of the basic details of the struggles that these persons were engaged in to protect the
+            Earth for
+            all of us to live on. The research sometimes will take the stories of these martyrs and compare them with
+            some
+            psychological attributes that the research team believe they have exhibited during their life. For example,
+            our
+            research suggests that Eco-martyrs exemplify Maslow’s concept of self-actualization and Kolhberg’s concept
+            of moral
+            development. More importantly, we draw attention to the fact that the people working to stop climate change
+            and
+            protect the environment are working at a heightened state of physical risk and deserve better protections by
+            authorities. Hopefully, our work will result in a book cataloging our eco-martyrs which we hope will
+            potentially
+            save lives.</p>
+    </div>
+
+
+    <h3>The Self-Actualization of Eco-Martyrs</h3>
+    <div class="resarchGrid">
+        <iframe src="https://drive.google.com/file/d/1kalNb292djzyz6i3CC3eh-3J02biLeuQ/preview" width="512" height="384"
+            allow="autoplay"></iframe>
+
+        <iframe src="https://drive.google.com/file/d/1XPX8osC2sbsnhC2--8YMfZfiawrm9ER8/preview" width="512" height="384"
+            allow="autoplay"></iframe>
+    </div>
+
+    <div class="maslowVideo">
+        <h3>Eco-Martyrs &amp Maslow</h3>
+        <iframe src="https://drive.google.com/file/d/1l-0fwCUhKODa6pdoQ5kvM_f6Ive8kxaq/preview" width="512" height="384"
+            allow="autoplay"></iframe>
+    </div>
+
+    <h3>Publications</h3>
+    <a href="https://journals.sagepub.com/doi/10.1177/09593543211040792">Bargdill, R., Lankford, A., Creed, R.,
+        Purrington, W., Rios-Santos, K. Latourrette, C.,
+        Guz-Montgomery, J.(2021). Eco-Martyrdom: Positive elements of self-sacrifice.
+        Theory and Psychology. doi.org/10.1177/09593543211040792</a>
+</div>
+
 <?php get_footer(); ?>
