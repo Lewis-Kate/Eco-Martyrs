c7fb26efcb31d238b2aac528807e680776eeb814
Kate Lewis <lew17020@byu.edu>
2023-01-31 19:07:25 -0500

    npm fixed

--- archives.php
+++ archives.php
@@ -5,9 +5,11 @@
     <h1>Archives</h1>
 </div>
 
-<div class="google_sheet">
+<!--<div class="google_sheet">
     <iframe src="https://docs.google.com/spreadsheets/d/1YRxXQI7Ae6HuZlAlZx2AkAAnjO37JinGC_HkXIbkKoo/edit#gid=0"
         width="1050" height="1200" allow="autoplay"></iframe>
-</div>
+</div>!-->
 
+
+<p style="text-align:center">Coming Soon</p>
 <?php get_footer(); ?>
