eaeca22ddc5978b34a22e32d7bfa8fa48eec0f4b
Kate Lewis <lew17020@byu.edu>
2023-01-31 19:05:51 -0500

    final updates and styling to eco martyrys before going to rampages

--- archive-archivegallery.php
+++ archive-archivegallery.php
@@ -1,20 +1,20 @@
 <?php /*Template Name: Gallery Archive */ 
 get_header(); ?>
 
-<h1 class="archive">Archive Gallery</h1>
+<h1 class="archive-title">Archive Gallery</h1>
 <!-- <div class="galleryButton">
 <button onclick="previousSlide()"><i class="fas fa-chevron-left fa-2x"></i></button>
 <button id="showNext" class="rightButton" onclick="nextSlide()"><i class="fas fa-chevron-right fa-2x"></i></button>
 </div>-->
 
-
+<div class="archive_grid">
 
 
  
     
 <?php echo presentSlide();?>
           
-            
+</div>  
             
             
 
