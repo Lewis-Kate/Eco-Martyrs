8081f4629c28cb194a99331ce27133e13a95e3fb
Kate Lewis <lewiskj2@vcu.edu>
2023-12-28 16:38:12 -0500

    Update archives.php

--- archives.php
+++ archives.php
@@ -3,13 +3,42 @@
 
 <div class="about">
     <h1>Archives</h1>
+</div><br/>
+
+<div class="google_sheet">
+    <h2>Eco-Martyrs Master Excel Document</h2>
+    <iframe src="https://docs.google.com/spreadsheets/d/1zWTr36OdKnEYR_VQMBctDHpTptF7rsX1QHfmSQmDVtw/edit?usp=drive_link"
+        width="1050" height="900" allow="autoplay"></iframe>
+</div><br/><br/>
+
+<div class="about">
+    <h1>Eco-Martyr Master Summaries</h1><br/>
 </div>
 
-<!--<div class="google_sheet">
-    <iframe src="https://docs.google.com/spreadsheets/d/1YRxXQI7Ae6HuZlAlZx2AkAAnjO37JinGC_HkXIbkKoo/edit#gid=0"
-        width="1050" height="1200" allow="autoplay"></iframe>
-</div>!-->
+<div class="google_sheet">
+    <h3>0-500 Names </h3>
+    <iframe src="https://docs.google.com/document/d/1Zgw-aYYmo28jDmcnYwQpa9E3gRC1CA5X/edit?usp=drive_link&ouid=118245124009826623739&rtpof=true&sd=true"
+        width="1050" height="600" allow="autoplay"></iframe>
+</div><br/>
+
+<div class="google_sheet">
+    <h3>500-1000 Names </h3>
+    <iframe src="https://docs.google.com/document/d/1s6DhAj16xy2ycVPyXr8MAKkKEBxTdfNk/edit?usp=drive_link&ouid=118245124009826623739&rtpof=true&sd=true"
+        width="1050" height="600" allow="autoplay"></iframe>
+</div><br/>
+
+<div class="google_sheet">
+    <h3>1000-1500 Names </h3>
+    <iframe src="https://docs.google.com/document/d/1q5Q4HpIeqENJTO9IBX52wTHObqOkgkMM/edit?usp=drive_link&ouid=118245124009826623739&rtpof=true&sd=true"
+        width="1050" height="600" allow="autoplay"></iframe>
+</div><br/>
+
+<div class="google_sheet">
+    <h3>1500-2000 Names </h3>
+    <iframe src="https://docs.google.com/document/d/1PU6ahew240vZ4gRW7XMbwaLLGe0ao8SY/edit?usp=drive_link&ouid=118245124009826623739&rtpof=true&sd=true"
+        width="1050" height="600" allow="autoplay"></iframe>
+</div>
+<br/>
 
 
-<p style="text-align:center">Coming Soon</p>
 <?php get_footer(); ?>
